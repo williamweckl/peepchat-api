ccbc23145e6b6f52d2929bb05aea6805bc8adcca
William Weckl <william.weckl@gmail.com>
2017-03-18 20:26:13 -0300

    test fixes

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -4,18 +4,28 @@ defmodule Peepchat.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  test "renders 401.json" do
+    assert render(Peepchat.ErrorView, "401.json", []) ==
+           %{"errors" => [%{code: 401, title: "Unauthorized"}], "jsonapi" => %{"version" => "1.0"}}
+  end
+
+  test "renders 403.json" do
+    assert render(Peepchat.ErrorView, "403.json", []) ==
+           %{"errors" => [%{code: 403, title: "Forbidden"}], "jsonapi" => %{"version" => "1.0"}}
+  end
+
   test "renders 404.json" do
     assert render(Peepchat.ErrorView, "404.json", []) ==
-           %{errors: %{detail: "Page not found"}}
+           %{"errors" => [%{code: 404, title: "Not found"}], "jsonapi" => %{"version" => "1.0"}}
   end
 
   test "render 500.json" do
     assert render(Peepchat.ErrorView, "500.json", []) ==
-           %{errors: %{detail: "Internal server error"}}
+           %{"errors" => [%{code: 500, title: "Internal server error"}], "jsonapi" => %{"version" => "1.0"}}
   end
 
   test "render any other" do
     assert render(Peepchat.ErrorView, "505.json", []) ==
-           %{errors: %{detail: "Internal server error"}}
+           %{"errors" => [%{code: 500, title: "Internal server error"}], "jsonapi" => %{"version" => "1.0"}}
   end
 end
